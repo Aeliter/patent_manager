188264d99186e5b553ba3a09f95ccb16d4126841
Aeliter <1047322903@qq.com>
2019-11-18 19:24:24 +0800

    实体类

--- patent_manager_entity/src/main/java/com/suixingpay/patent/entity/TargetPatent.java
+++ patent_manager_entity/src/main/java/com/suixingpay/patent/entity/TargetPatent.java
@@ -10,8 +10,14 @@ import lombok.Data;
  */
 @Data
 public class TargetPatent {
+    /** 指标专利编号 **/
+    private Long tpId;
     /** 指标编号 **/
     private Long targetId;
     /** 专利编号 **/
     private Long patentId;
+    /**指标实体类**/
+    private Target target;
+    /***专利实体类*/
+    private Patent patent;
 }
