c4f8464d1514d569db94081737ceea78bb9e38c5
Aeliter <1047322903@qq.com>
2019-11-18 19:02:37 +0800

    实体类

--- patent_manager_entity/src/main/java/com/suixingpay/patent/entity/Patent.java
+++ patent_manager_entity/src/main/java/com/suixingpay/patent/entity/Patent.java
@@ -11,29 +11,29 @@ import java.util.Date;
  **/
 @Data
 public class Patent {
-        /** 专利表主键 **/
-        private Long patentId;
-        /** 专利名称 **/
-        private String patentName;
-        /** 专利号 **/
-        private String patentNumber;
-        /** 批次 **/
-        private String patentBatch;
-        /** 专利申请号 **/
-        private String patentRequestNumber;
-        /** 申请日期 **/
-        private Date patentDate;
-        /** 技术负责人 **/
-        private Integer userIdTechnology;
-        /** 专利新建人 **/
-        private Integer userIdNew;
-        /** 专利（公司）中文名称 **/
-        private String patentApplicant;
-        /** 专利类型 **/
-        private Integer patentType;
-        /** 法律状态 **/
-        private Integer patentLawState;
-        /** 专利备注 **/
-        private String patentComment;
+    /** 专利表主键 **/
+    private Long patentId;
+    /** 专利名称 **/
+    private String patentName;
+    /** 专利号 **/
+    private String patentNumber;
+    /** 批次 **/
+    private String patentBatch;
+    /** 专利申请号 **/
+    private String patentRequestNumber;
+    /** 申请日期 **/
+    private Date patentDate;
+    /** 技术负责人 **/
+    private Integer userIdTechnology;
+    /** 专利新建人 **/
+    private Integer userIdNew;
+    /** 专利（公司）中文名称 **/
+    private String patentApplicant;
+    /** 专利类型 **/
+    private Integer patentType;
+    /** 法律状态 **/
+    private Integer patentLawState;
+    /** 专利备注 **/
+    private String patentComment;
 
 }
---
+++ patent_manager_entity/src/main/java/com/suixingpay/patent/entity/Target_patent.java
@@ -0,0 +1,17 @@
+package com.suixingpay.patent.entity;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @author: Adminstrator
+ * @date: 2019-11-18 18:05
+ * @version: V1.0
+ */
+@Data
+public class Target_patent {
+    /** 指标编号 **/
+    private Long targetId;
+    /** 专利编号 **/
+    private Long patentId;
+}
