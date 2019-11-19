e6daf0f2997a9664524054bfdc0883880406b2fb
yufengv <1013842037@qq.com>
2019-11-19 20:49:34 +0800

    登录功能模块

--- patent_manager_tp/src/main/java/com/suixingpay/tp/service/UserService.java
+++ patent_manager_tp/src/main/java/com/suixingpay/tp/service/UserService.java
@@ -1,4 +1,8 @@
 package com.suixingpay.tp.service;
 
+import com.suixingpay.patent.entity.User;
+import org.apache.ibatis.annotations.Param;
+
 public interface UserService {
+    User login(@Param("userStaff") String userStaff, @Param("passWord") String passWord);
 }
--- patent_manager_tp/src/main/java/com/suixingpay/tp/service/impl/UserServiceImpl.java
+++ patent_manager_tp/src/main/java/com/suixingpay/tp/service/impl/UserServiceImpl.java
@@ -1,6 +1,22 @@
 package com.suixingpay.tp.service.impl;
 
+import com.suixingpay.patent.entity.User;
+import com.suixingpay.tools.MD5Util;
+import com.suixingpay.tp.mapper.UserMapper;
 import com.suixingpay.tp.service.UserService;
+import org.apache.catalina.mbeans.UserMBean;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper  userMapper;
+   public User login(@Param("userStaff")String userStaff,@Param("passWord")String passWord)
+   {
+       //使用md5对密码加密
+       String passwordMd5 = MD5Util.MD5Encode(passWord, "UTF-8");
+       return userMapper.login(userStaff,passwordMd5);
+   }
 }
