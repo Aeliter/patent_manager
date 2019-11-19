f02ba4790651f478a0ebe37979b4f31bbd48e705
superiorwilliam <y18045360690@163.com>
2019-11-19 17:18:00 +0800

    编写新建专利，检查

--- patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
+++ patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
@@ -2,6 +2,8 @@ package com.suixingpay.tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description:
@@ -9,7 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date: 2019-11-19 11:36
  * @version: V1.0
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.suixingpay.tp.mapper"})
 public class TpApplication {
     public static void main(String[] args) {
         SpringApplication.run(TpApplication.class, args);
