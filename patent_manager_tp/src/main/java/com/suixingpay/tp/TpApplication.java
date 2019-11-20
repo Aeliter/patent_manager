020e767da53e8d55d164768fa6d8244a9feb1337
Aeliter <1047322903@qq.com>
2019-11-20 09:27:11 +0800

    user

--- patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
+++ patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
@@ -1,10 +1,7 @@
 package com.suixingpay.tp;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description:
