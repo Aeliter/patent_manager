2151a825af474a1babeb64e161a2cef6089d0616
Aeliter <1047322903@qq.com>
2019-11-20 09:21:21 +0800

    user

--- patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
+++ patent_manager_tp/src/main/java/com/suixingpay/tp/TpApplication.java
@@ -2,8 +2,6 @@ package com.suixingpay.tp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description:
@@ -11,8 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @date: 2019-11-19 11:36
  * @version: V1.0
  */
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.suixingpay.tp.mapper"})
+@SpringBootApplication
 public class TpApplication {
     public static void main(String[] args) {
         SpringApplication.run(TpApplication.class, args);
